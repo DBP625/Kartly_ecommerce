9cccefc2dfd11c0ebadff1f241afe99732814f83
Denesh Barua Pantho <99339590+DBP625@users.noreply.github.com>
2025-11-28 14:16:58 +0600

    Refactor server.js for CORS and middleware cleanup
    Removed duplicate middleware for parsing JSON and URL-encoded data. Added CORS middleware import and configuration.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -9,14 +10,13 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 
-import cors from "cors"; // To handle Cross-Origin Resource Sharing
 import { connectDB } from "./database/db.js";
 import "./database/firebase.admin.js";
 
+dotenv.config();
+
 const app = express();
-
-const __dirname = path.resolve(); // it means root directory of project
-
+const __dirname = path.resolve();
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -33,38 +33,18 @@ app.use(
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-
-
-// Middleware to parse JSON and URL-encoded data
-app.use(express.json({ limit: "50mb" })); // Parse JSON request bodies with 50mb limit
-app.use(express.urlencoded({ extended: true, limit: "50mb" })); // Parse URL-encoded data with 50mb limit
-app.use(cookieParser()); // Parse cookies
-
-// Debug middleware to log request body
-app.use((req, res, next) => {
-  console.log("Request Body:", req.body);
-  next();
-});
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//   app.use((req, res, next) => {
-//     if (req.path.startsWith("/api")) return next();
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
